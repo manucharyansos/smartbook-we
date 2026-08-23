fa23c3d3fdf2781cb8655dfed4b3933d8f33c6bd
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-08-24 03:34:17 +0400

    Load premium authentication styles

--- src/main.tsx
+++ src/main.tsx
@@ -26,6 +26,7 @@ import "./index.css";
 import "./premium-home.css";
 import "./premium-public.css";
 import "./premium-marketing.css";
+import "./premium-auth.css";
 import { LanguageProvider } from "./contexts/LanguageContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { AppErrorBoundary } from "./components/AppErrorBoundary";
